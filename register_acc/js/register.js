ba188aabfb42c48c4847168eb4bb3dfabeff6407
bendoantran <137751169+bendoantran@users.noreply.github.com>
2023-12-22 21:09:53 +0700

    Add files via upload

---
+++ register_acc/js/register.js
@@ -0,0 +1,8 @@
+function login() {
+        var fullName = document.getElementById('fullName').value;
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        // Perform signup logic (add server-side validation and storage in a real application)
+        alert('Sign Up successful for ' + fullName);
+    }
